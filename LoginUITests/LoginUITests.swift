6ed8ba689cda65e0716b94247c3c56bda96062d2
Yasir Siddique <imyasir34@gmail.com>
2021-11-04 00:52:19 -0700

    Initial Commit

---
+++ LoginUITests/LoginUITests.swift
@@ -0,0 +1,34 @@
+//
+//  LoginUITests.swift
+//  LoginUITests
+//
+//  Created by Yasir Siddique on 04/11/2021.
+//  Copyright © 2021 Yasir Siddique. All rights reserved.
+//
+
+import XCTest
+@testable import LoginUI
+
+class LoginUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
